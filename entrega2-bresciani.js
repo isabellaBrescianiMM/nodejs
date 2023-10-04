992a63240ffc12444eba70928389abd4a791025b
Isabella Bresciani <isabellabresciani@MacBook-Air-de-Isabella.local>
2023-10-03 21:05:00 -0300

    entrega 3

--- entrega2-bresciani.js
+++ entrega2-bresciani.js
@@ -1,72 +1,102 @@
-const fs = require('fs')
+const fs = require('fs');
 
-class ProductManager{
-
-    constructor(path){
-        this.path = path
+class ProductManager {
+    constructor(path) {
+        this.path = path;
     }
 
-    getNextID = (list) =>{
-        return (list.length>0) ? list[list.length-1].id + 1 : 1
+    async getNextID(list) {
+        return list.length > 0 ? list[list.length - 1].id + 1 : 1;
     }
-    read = async () =>{
-        if(fs.existsSync(this.path)){
-            return fs.promises.readFile(this.path, 'utf-8').then(r => JSON.parse(r))
+
+    async read() {
+        if (fs.existsSync(this.path)) {
+            const data = await fs.promises.readFile(this.path, 'utf-8');
+            return JSON.parse(data);
+        } else {
+            return [];
         }
-        
-    }
-    write = list =>{
-        fs.promises.writeFile(this.path, JSON.stringify(list))
     }
 
-    getProducts = async () =>{
-        const data = await this.read()
-        return data
+    async write(list) {
+        await fs.promises.writeFile(this.path, JSON.stringify(list, null, 2));
     }
 
-    addProduct = async (obj) =>{
-        const list = await this.read()
-        const getNextID = this.getNextID(list)
-        obj.id = NextID
-        list.push(obj)
-        await this.write(list)
-        return obj
+    async getProducts() {
+        const data = await this.read();
+        return data;
     }
 
-    updateProduct = async (id,obj) => {
-        obj.id = id
-        const list = await this.read()
-        for(let i=0; i<list.length; i++){
-            if(list[i].id == id){
-                list[i]=obj
-                break
-            }
+    async addProduct(obj) {
+        const list = await this.read();
+
+        // Validar que se reciban todos los parámetros esperados
+        if (!obj.title || !obj.description || !obj.price || !obj.thumbnail || !obj.code || obj.stock === undefined) {
+            throw new Error('Faltan parámetros en el objeto del producto');
         }
-        await this.write(list)
+
+        // Validar que el código no se repita
+        if (list.some(product => product.code === obj.code)) {
+            throw new Error('El código del producto ya existe');
+        }
+
+        const nextID = await this.getNextID(list);
+        obj.id = nextID;
+        list.push(obj);
+        await this.write(list);
+        return obj;
     }
-    updateProductIdx = async (id,obj) => {
-        obj.id = id
-        const list = await this.read()
-        const idx = list.findIndex(e => e.id == id)
-        if(idx<1)return
-        list[idx] = obj
-        
-        await this.write(list)
-    }
-    getElementById = (id)=>{
-        let elem
-        this.list.forEach(element => {
-            if(element.id == id){
-                elem = element
-            }else{
-                console.log("El elemento no fue encontrado")
+
+    async updateProduct(id, updatedProduct) {
+        const list = await this.read();
+        const index = list.findIndex(product => product.id === id);
+
+        if (index !== -1) {
+            // Validar que solo se actualicen las propiedades del producto
+            const allowedProps = ['title', 'description', 'price', 'thumbnail', 'code', 'stock'];
+            const updatedProps = Object.keys(updatedProduct);
+
+            // Verificar si hay propiedades no permitidas en updatedProduct
+            const invalidProps = updatedProps.filter(prop => !allowedProps.includes(prop));
+
+            if (invalidProps.length > 0) {
+                throw new Error('Propiedades no válidas en el objeto de producto');
             }
-        })
-        return elem
+
+            // Verificar si el código del producto ya existe
+            const existingProductWithCode = list.find(product => product.code === updatedProduct.code && product.id !== id);
+
+            if (existingProductWithCode) {
+                throw new Error('El código del producto ya existe');
+            }
+
+            // Actualizar solo las propiedades permitidas
+            list[index] = { ...list[index], ...updatedProduct };
+
+            await this.write(list);
+        } else {
+            throw new Error('Producto no encontrado');
+        }
     }
 
+    async deleteProduct(id) {
+        const list = await this.read();
+        const index = list.findIndex(product => product.id === id);
+        if (index !== -1) {
+            list.splice(index, 1);
+            await this.write(list);
+        } else {
+            throw new Error('Producto no encontrado');
+        }
+    }
 
+    async getProductById(id) {
+        const list = await this.read();
+        const product = list.find(product => product.id === id);
+        if (product) {
+            return product;
+        } else {
+            throw new Error('Producto no encontrado');
+        }
+    }
 }
-
-
-module.exports = ProductManager
